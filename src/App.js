8aa381fff19b03da8e739f991ba2560022f6e63c
Kone <kone@gmail.com>
2022-03-23 13:57:10 +0100

    hello

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <img src="/assets/image/m1.jpg" width={100} height={100} alt="" />
+    </div>
+  );
+}
+
+export default App;
